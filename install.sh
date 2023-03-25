a6943b458f9c9fa6799a30637fcb5a6561f9bc76
Bart Slegt <109470782+Euforic-Coder@users.noreply.github.com>
2023-03-25 15:50:38 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+
+if command -v cmake
+then
+	cmake CMakeLists.txt
+fi
+
+if command -v make
+then
+	sudo make install
+fi
+
+sudo mkdir -p /etc/xtracter
+sudo cp english.dic /etc/xtracter

aa3f39f00c80358f47b7eff4fcd9c290f17e3fde
Bart Slegt <109470782+Euforic-Coder@users.noreply.github.com>
2023-03-25 08:01:50 +0100

    Building program

--- main.cpp
+++ main.cpp
@@ -1,46 +1,38 @@
 #include <iostream>
 #include <getopt.h>
 #include <fstream>
+#include <algorithm>
+#include <chrono>
 #include "uforia/uforia.h"
 
-#define DEBUG 0
+#define DEBUG 1
 #if DEBUG
-#define LOG(x) cout << "Debug: " << x << endl
-#define COMPARE(x, y) cout << colored(x, Red, Italic) << " " << colored(y, Blue, Italic) << endl
+#define LOG(x) cout << colored(x, Yellow, Bold) << endl
 #else
 #define LOG(x)
-#define COMPARE(x, y)
 #endif
 
 using namespace std;
 
-int maximal_chars = 12, minimal_chars = 4;
+int maximal_chars = 12, minimal_chars = 6;
 ifstream file;
 ofstream write;
-string language;
+ofstream unextracted;
+string buffer;
 vector<string> dictionary = from("english.dic", true);
 vector<string> collection;
-bool flag_file, flag_write, flag_verbose;
-
-// Set the dictionary language
-void set_dictionary(string language){
-    // TODO: Add languages
-    if(language == "english"){
-        dictionary = from("english.dic", true);
-        LOG("Dictionary is set to English");
-    }else if(language == "spanish"){
-        dictionary = from("spanish.dic", false);
-    }else{
-        error("Wrong language is given", true);
-    }
-}
+bool flag_file = false, flag_write = false, flag_unextracted = false, flag_verbose = true;
 
 // Extract words from the password
 int index = 0;
 void extract(string password){
+    string unextract;
     for(unsigned short int i = 0; i < dictionary.size(); ++i){
-        string word = dictionary[i];
-	if(has(word, password) && word.length() < password.length() && word.length() <= maximal_chars && word.length() >= minimal_chars){
+        const string& word = dictionary[i];
+        if((word.length() <= maximal_chars && word.length() >= minimal_chars && password == word)
+                || (has(word, password) && word.length() < password.length() && word.length()
+                    <= maximal_chars && word.length() >= minimal_chars)){
+
             collection.push_back(word);
 
             // Print words in 6 columns
@@ -61,22 +53,81 @@ void extract(string password){
 
             // The found word is removed from the password and reused
             password.erase(password.find(word), word.length());
-            COMPARE(word, password);
             extract(password);
+        }else if(flag_unextracted){
+            unextract = password;
         }
+    }
+    if(flag_unextracted && !unextract.empty()){
+        unextracted << password << endl;
+
+    }
+}
+
+struct Extract{
+    Extract(string s){
+        this->word = s;
+    }
+    string word;
+    int count = 1;
+};
+
+vector<Extract> extracted;
+void count(){
+    for(unsigned short i = 0; i < collection.size(); i++){
+        const string collect = collection[i];
+
+        // Add the first extract
+        if(extracted.empty()){
+            Extract extract(collect);
+            extracted.push_back(extract);
+            continue;
+        }
+
+        // Check if te collected word is present in extracted
+        for(unsigned short j = 0; j < extracted.size(); ++j){
+            const Extract extract = extracted[j];
+            if(extract.word == collect){
+                int c = extract.count;
+                Extract e(collect);
+                e.count = c + 1;
+                extracted.erase(extracted.begin() + j);
+                extracted.emplace_back(e);
+            }
+        }
+
+        // Add an Extract to extracted
+        Extract e(collect);
+        extracted.push_back(e);
+    }
+
+    // Sort extracted by frequence
+    sort(extracted.begin(), extracted.end(), [](Extract a, Extract b){
+        return a.count > b.count;
+    });
+
+    // Write results
+    cout << endl << endl;
+    if(flag_write){
+        for(unsigned short k = 0; k < extracted.size(); k++){
+            Extract extract = extracted[k];
+            write << extract.word << endl;
+        }
+        write.close();
     }
 }
 
 void help(){
-    HelpMaker help("xtracter");
+    HelpMenu help("xtracter");
     help.add_usage("xtracter -f <FILE> -o <FILE> [OPTIONS]");
-    help.add_argument("-f", "Define the password file");
-    help.add_argument("-o", "Define the write file");
-    help.add_argument("-l", "Define the language");
-    help.add_argument("-m", "Define the minimal length of characters");
-    help.add_argument("-M", "Define the maximal length of characters");
-    help.add_argument("-v", "Set verbosity");
-    help.add_example("xtracter -f rockyou.txt -o words.txt -l english -m 4 -M 10 -v");
+    help.add_option("-f", "Define the password file", true);
+    help.add_option("-o", "Define the write file", true);
+    help.add_option("-m", "Define the minimal length of characters", false);
+    help.add_option("-M", "Define the maximal length of characters", false);
+    help.add_option("-u", "Define the write file for unextracted passwords", false);
+    help.add_option("-v", "Set verbosity off", false);
+    help.add_option("-h", "Show help menu", false);
+    help.add_example("xtracter -f rockyou.txt -o words.txt -m 4 -M 10");
     help.print();
 }
 
@@ -89,17 +140,14 @@ int main(int argc, char* argv[])
     // Getopt
     int c;
 
-    while ((c = getopt (argc, argv, "f:l:o:m:M:v")) != -1)
+    while ((c = getopt (argc, argv, "f:o:m:M:u:vh")) != -1)
         switch (c)
         {
-        case 'f':
+        case 'f':{
             file.open(optarg);
             flag_file = true;
             LOG(optarg);
-            break;
-        case 'l':
-            language = optarg;
-            LOG(optarg);
+        }
             break;
         case 'o':
             write.open(optarg);
@@ -112,8 +160,16 @@ int main(int argc, char* argv[])
         case 'M':
             maximal_chars = stoi(optarg);
             break;
+        case 'u':
+            unextracted.open(optarg);
+            flag_unextracted = true;
+            break;
         case 'v':
-            flag_verbose = true;
+            flag_verbose = false;
+            break;
+        case 'h':
+            help();
+            exit(0);
             break;
         default:
             abort();
@@ -124,11 +180,30 @@ int main(int argc, char* argv[])
         error("Failed to open the dictionary", true);
     }
 
-    // Extract words
     string password;
+
+#if DEBUG
+    auto start = std::chrono::high_resolution_clock::now();
+#endif
+
+    // Extract words
     while(file >> password){
         extract(lowercase(password));
     }
+    cout << endl;
+    file.close();
+    if(flag_unextracted){
+        unextracted.close();
+    }
 
+    // Count words
+    count();
+
+#if DEBUG
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = finish - start;
+    string message = "Elapsed Time: " + to_string(elapsed.count()) + " seconds";
+    cout << colored(message, Magenta, Bold) << endl;
+#endif
     return 0;
 }

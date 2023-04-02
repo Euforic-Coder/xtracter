81dbeac66a28f67d56312438d065322bc7c5747d
Bart Slegt <109470782+Euforic-Coder@users.noreply.github.com>
2023-04-02 11:00:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ sudo cmake CMakeLists.txt
 sudo make install
 sudo mkdir -p etc/xtracter
 sudo cp english.dic /etc/xtracter
+sudo cp dutch.dic /etc/xtracter
+sudo cp spanish.dic /etc/xtracter
+sudo cp french.dic /etc/xtracter
+sudo cp german.dic /etc/xtracter
